4b1a63be115d238b9cc48baa6ec9e96129dc8681
yogiefajr <yogie.fajar15@gmail.com>
2016-11-07 09:05:43 +0700

    Migration Eloquent COntroller

--- app/Http/Controllers/controllerJadwal_Imunisasi_Bayi.php
+++ app/Http/Controllers/controllerJadwal_Imunisasi_Bayi.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class controllerJadwal_Imunisasi_Bayi extends Controller
 {
-    //
+    public function show($id){
+     return view('user.profile',['user'=> use::findOrFail($id)]);
+    }
+
+    public function show($lihat_jadwal_imunisasi){
+     return view('user.profile',['user'=> use::findOrFail($jadwal)]);
+    }
 }
