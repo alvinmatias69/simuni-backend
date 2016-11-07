f969f6dd8465db23173d03d777e3eb09c3a09865
arinlupin <arinilupin@gmail.com>
2016-11-07 08:53:09 +0700

    Penambahan Model, View, dan Migration Bidan

---
+++ app/Http/Controllers/ControllerBidan.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ControllerBidan extends Controller
+{
+    public function register_bayi()
+    {
+    	return 
+    }
+
+}
---
+++ database/migrations/2016_11_07_005209_CreateTableBidan.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTabelBidan extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Bidan', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama_bidan');
+            $table->string('id_bidan');
+            $table->string('username');
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Bidan');
+    }
+}
